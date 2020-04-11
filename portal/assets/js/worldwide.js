1b96cec3fdefeadf9da746749d6786ff2f7696f4
Matt Harrington <matt2harrington@gmail.com>
2020-04-11 17:18:00 -0400

    updated sql and started work on pie chart for percentages

---
+++ portal/assets/js/worldwide.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+  var dataResults = [];
+  // var timestamps = [];
+  var deaths = [];
+  var countries = [];
+  // var countries = [];
+  $.ajax({
+      url: 'http://localhost:10000/corona',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      type: "GET", /* or type:"GET" or type:"PUT" */
+      dataType: "json",
+      data: {
+      },
+      success: function (result) {
+          dataResults.push(result);
+
+      },
+      error: function () {
+          console.log("error");
+      }
+  });
+
+  setTimeout(function() {
+    var i;
+    for (i = 0; i < dataResults[0]['DataList'].length; i++) {
+      countries.push(dataResults[0]['DataList'][i].Country)
+      // timestamps.push(dataResults[0]['DataList'][i].Updated)
+      deaths.push(dataResults[0]['DataList'][i].Deaths)
+      var ctx = document.getElementById('worldwideDeaths').getContext('2d');
+      var chart = new Chart(ctx, {
+          // The type of chart we want to create
+          type: 'pie',
+
+          // The data for our dataset
+          data: {
+              labels: countries,
+              datasets: [{
+                  label: 'Worldwide Death Percentage',
+                  borderColor: 'rgb(255, 99, 132)',
+                  data: deaths
+              }]
+          },
+
+          // Configuration options go here
+          options: {
+            fill: false
+          }
+      });
+    }
+      // console.log(dataResults[0]['DataList'][0].length)
+  },1000);
+})
