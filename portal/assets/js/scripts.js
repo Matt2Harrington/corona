7dbaf14bb73e5b28dd3504354c99852b14725175
Matt Harrington <matt2harrington@gmail.com>
2020-04-11 17:31:00 -0400

    working with worldwide but commented out for perforance as-is

--- portal/assets/js/scripts.js
+++ portal/assets/js/scripts.js
@@ -54,37 +54,4 @@ $(document).ready(function () {
     }
       // console.log(dataResults[0]['DataList'][0].length)
   },1000);
-
-  // setTimeout(function() {
-  //   var i;
-  //   for (i = 0; i < dataResults[0]['DataList'].length; i++) {
-  //     if (dataResults[0]['DataList'][i].Country === 'USA') {
-  //       timestamps.push(dataResults[0]['DataList'][i].Updated)
-  //       deaths.push(dataResults[0]['DataList'][i].Deaths)
-  //       var ctx = document.getElementById('deathsChart').getContext('2d');
-  //       var chart = new Chart(ctx, {
-  //           // The type of chart we want to create
-  //           type: 'line',
-  //
-  //           // The data for our dataset
-  //           data: {
-  //               labels: timestamps,
-  //               datasets: [{
-  //                   label: 'United States Deaths by COVID-19',
-  //                   borderColor: 'rgb(255, 99, 132)',
-  //                   data: cases
-  //               }]
-  //           },
-  //
-  //           // Configuration options go here
-  //           options: {
-  //             fill: false
-  //           }
-  //       });
-  //     } else {
-  //       // console.log(dataResults[0]['DataList'][i].ID)
-  //     }
-  //   }
-  //     // console.log(dataResults[0]['DataList'][0].length)
-  // },1000);
 })
